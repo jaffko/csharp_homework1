94c85c9fbf5fdfa7e652046dffdbcebf42391979
Ildar <steel14@yandex.ru>
2022-08-31 23:38:48 +0500

    added taks1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите 2 числа: ");
+try
+{
+    int first_num = Convert.ToInt32(Console.ReadLine());
+    int second_num = Convert.ToInt32(Console.ReadLine());
+    if (first_num > second_num)
+        Console.WriteLine("Макисмальное -> " + first_num);
+    else if (second_num > first_num)
+        Console.WriteLine("Максимальное -> " + second_num);
+    else Console.WriteLine("Числа равны"); 
+}
+catch
+{
+    
+    Console.WriteLine("Нужно число");
+}
