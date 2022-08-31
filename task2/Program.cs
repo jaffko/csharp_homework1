af83a5f7069827045e460fdf53614c8f67001e9c
Ildar <steel14@yandex.ru>
2022-08-31 23:45:06 +0500

    added taks2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите три числа: ");
+try
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int c = Convert.ToInt32(Console.ReadLine());
+    int max = a;
+    if (b > max)
+        max = b;
+    if (c > max)
+        max = c;
+    Console.WriteLine("Максимальное -> " + max);
+}
+catch 
+{
+    Console.WriteLine("Нужно число");
+}
