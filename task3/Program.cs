a5c388d5acc109bdab19a9f6fe5f1ae545a076fe
Ildar <steel14@yandex.ru>
2022-08-31 23:47:20 +0500

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+try
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (a % 2 == 0) Console.WriteLine("Четное");
+    else Console.WriteLine("Нечетное");
+}
+catch 
+{
+    Console.WriteLine("Нужно число");
+}
