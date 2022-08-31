c823b66962d2b2cd6d0615220712d1592aefa1e9
Ildar <steel14@yandex.ru>
2022-08-31 23:59:30 +0500

    added task4

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите положительное число: ");
+try
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (a <= 0) Console.WriteLine("Нужно положительное число");
+    else 
+    {
+        int i = 4;
+        if (a >= 2) Console.Write(2);
+        while (i <=a)
+            {
+                Console.Write(", " + i);
+                i = i + 2;         
+            }
+    }
+}
+catch 
+{
+    Console.WriteLine("Нужно число");
+}
